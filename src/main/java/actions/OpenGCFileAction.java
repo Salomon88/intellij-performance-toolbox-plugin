8940ed49898f33c333716c2ad2ba35c27cbba42a
Nikolay Khramchenkov <xnike@xnike.me>
2020-06-27 16:04:11 +0300

    Gradle 6.5

--- src/main/java/actions/OpenGCFileAction.java
+++ src/main/java/actions/OpenGCFileAction.java
@@ -2,13 +2,22 @@ package actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.tagtraum.perf.gcviewer.GCViewer;
 import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class OpenGCFileAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        new GCViewer().main(new String[]{});
+        new Thread(() -> {
+            try {
+                new GCViewer().doMain(new String[]{((VirtualFile) e.getDataContext().getData("virtualFile")).getCanonicalPath()});
+            } catch (InvocationTargetException | InterruptedException exception) {
+                exception.printStackTrace();
+            }
+        }).start();
     }
 }
