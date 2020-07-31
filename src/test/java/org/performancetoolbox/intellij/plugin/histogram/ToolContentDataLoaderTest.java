992936f16714e74c4619b5826a23380d3c828699
Nikolay Khramchenkov <xnike@xnike.me>
2020-07-31 10:48:08 +0300

    #18

--- src/test/java/org/performancetoolbox/intellij/plugin/histogram/ToolContentDataLoaderTest.java
+++ src/test/java/org/performancetoolbox/intellij/plugin/histogram/ToolContentDataLoaderTest.java
@@ -8,15 +8,17 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 import static java.lang.String.format;
+import static java.nio.file.Files.writeString;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class ToolContentDataLoaderTest {
 
@@ -31,7 +33,7 @@ public class ToolContentDataLoaderTest {
         long size = 1024;
 
         File file = temporaryFolder.newFile("histogram");
-        Files.writeString(file.toPath(), format("1: %d %d %s (%s)", instances, size, name, module));
+        writeString(file.toPath(), format("1: %d %d %s (%s)", instances, size, name, module));
         ToolContentDataLoader dataLoader = new ToolContentDataLoader(singletonList(new LightVirtualFile(file.getPath())));
         dataLoader.execute();
 
@@ -52,12 +54,12 @@ public class ToolContentDataLoaderTest {
                 new LightVirtualFile(temporaryFolder.newFile("4").getPath()),
                 new LightVirtualFile(temporaryFolder.newFile("5").getPath()),
                 new LightVirtualFile(temporaryFolder.newFile("6").getPath()));
-        Files.writeString(Paths.get(files.get(0).getPath()), "1: 10 20 testName");
-        Files.writeString(Paths.get(files.get(1).getPath()), "");
-        Files.writeString(Paths.get(files.get(2).getPath()), "1: 5 10 testName");
-        Files.writeString(Paths.get(files.get(3).getPath()), "");
-        Files.writeString(Paths.get(files.get(4).getPath()), "1: 20 40 testName");
-        Files.writeString(Paths.get(files.get(5).getPath()), "1: 40 80 testName");
+        writeString(Paths.get(files.get(0).getPath()), "1: 10 20 testName");
+        writeString(Paths.get(files.get(1).getPath()), "");
+        writeString(Paths.get(files.get(2).getPath()), "1: 5 10 testName");
+        writeString(Paths.get(files.get(3).getPath()), "");
+        writeString(Paths.get(files.get(4).getPath()), "1: 20 40 testName");
+        writeString(Paths.get(files.get(5).getPath()), "1: 40 80 testName");
 
         ToolContentDataLoader dataLoader = new ToolContentDataLoader(files);
         dataLoader.execute();
@@ -66,21 +68,11 @@ public class ToolContentDataLoaderTest {
 
         assertEquals(10L, (long) dataLoader.getContentData().get(0).getInitialInstances());
         assertEquals(40L, (long) dataLoader.getContentData().get(0).getFinalInstances());
-        assertEquals(files.size() - 1, dataLoader.getContentData().get(0).getDifferencesInstances().length);
-        assertEquals(null, dataLoader.getContentData().get(0).getDifferencesInstances()[0]);
-        assertEquals(-5L, (long) dataLoader.getContentData().get(0).getDifferencesInstances()[1]);
-        assertEquals(null, dataLoader.getContentData().get(0).getDifferencesInstances()[2]);
-        assertEquals(15L, (long) dataLoader.getContentData().get(0).getDifferencesInstances()[3]);
-        assertEquals(20L, (long) dataLoader.getContentData().get(0).getDifferencesInstances()[4]);
+        assertArrayEquals(new Long[]{null, -5L, null, 15L, 20L}, dataLoader.getContentData().get(0).getDifferencesInstances());
 
         assertEquals(20L, (long) dataLoader.getContentData().get(0).getInitialSize());
         assertEquals(80L, (long) dataLoader.getContentData().get(0).getFinalSize());
-        assertEquals(files.size() - 1, dataLoader.getContentData().get(0).getDifferencesSizes().length);
-        assertEquals(null, dataLoader.getContentData().get(0).getDifferencesSizes()[0]);
-        assertEquals(-10L, (long) dataLoader.getContentData().get(0).getDifferencesSizes()[1]);
-        assertEquals(null, dataLoader.getContentData().get(0).getDifferencesSizes()[2]);
-        assertEquals(30L, (long) dataLoader.getContentData().get(0).getDifferencesSizes()[3]);
-        assertEquals(40L, (long) dataLoader.getContentData().get(0).getDifferencesSizes()[4]);
+        assertArrayEquals(new Long[]{null, -10L, null, 30L, 40L}, dataLoader.getContentData().get(0).getDifferencesSizes());
     }
 
     @Test
@@ -89,25 +81,21 @@ public class ToolContentDataLoaderTest {
                 new LightVirtualFile(temporaryFolder.newFile("1").getPath()),
                 new LightVirtualFile(temporaryFolder.newFile("2").getPath()),
                 new LightVirtualFile(temporaryFolder.newFile("3").getPath()));
-        Files.writeString(Paths.get(files.get(0).getPath()), "");
-        Files.writeString(Paths.get(files.get(1).getPath()), "1: 10 20 testName");
-        Files.writeString(Paths.get(files.get(2).getPath()), "1: 20 40 testName");
+        writeString(Paths.get(files.get(0).getPath()), "");
+        writeString(Paths.get(files.get(1).getPath()), "1: 10 20 testName");
+        writeString(Paths.get(files.get(2).getPath()), "1: 20 40 testName");
 
         ToolContentDataLoader dataLoader = new ToolContentDataLoader(files);
         dataLoader.execute();
 
         assertEquals(1, dataLoader.getContentData().size());
 
-        assertEquals(null, dataLoader.getContentData().get(0).getInitialInstances());
+        assertNull(dataLoader.getContentData().get(0).getInitialInstances());
         assertEquals(20L, (long) dataLoader.getContentData().get(0).getFinalInstances());
-        assertEquals(files.size() - 1, dataLoader.getContentData().get(0).getDifferencesInstances().length);
-        assertEquals(10L, (long) dataLoader.getContentData().get(0).getDifferencesInstances()[0]);
-        assertEquals(10L, (long) dataLoader.getContentData().get(0).getDifferencesInstances()[1]);
+        assertArrayEquals(new Long[]{10L, 10L}, dataLoader.getContentData().get(0).getDifferencesInstances());
 
-        assertEquals(null, dataLoader.getContentData().get(0).getInitialSize());
+        assertNull(dataLoader.getContentData().get(0).getInitialSize());
         assertEquals(40L, (long) dataLoader.getContentData().get(0).getFinalSize());
-        assertEquals(files.size() - 1, dataLoader.getContentData().get(0).getDifferencesSizes().length);
-        assertEquals(20L, (long) dataLoader.getContentData().get(0).getDifferencesSizes()[0]);
-        assertEquals(20L, (long) dataLoader.getContentData().get(0).getDifferencesSizes()[1]);
+        assertArrayEquals(new Long[]{20L, 20L}, dataLoader.getContentData().get(0).getDifferencesSizes());
     }
 }
