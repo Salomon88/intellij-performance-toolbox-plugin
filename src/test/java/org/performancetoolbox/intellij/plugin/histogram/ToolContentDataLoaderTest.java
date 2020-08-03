208393ac6b5698f84c2fc940befcbfbb29f4bd8d
Nikolay Khramchenkov <xnike@xnike.me>
2020-08-03 09:41:40 +0300

    junit5

--- src/test/java/org/performancetoolbox/intellij/plugin/histogram/ToolContentDataLoaderTest.java
+++ src/test/java/org/performancetoolbox/intellij/plugin/histogram/ToolContentDataLoaderTest.java
@@ -2,28 +2,29 @@ package org.performancetoolbox.intellij.plugin.histogram;
 
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.LightVirtualFile;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 import static java.lang.String.format;
 import static java.nio.file.Files.writeString;
-import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ToolContentDataLoaderTest {
 
-    @Rule
-    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+    @TempDir
+    public Path tempDir;
 
     @Test
     public void sanity() throws IOException {
@@ -32,7 +33,7 @@ public class ToolContentDataLoaderTest {
         long instances = 128;
         long size = 1024;
 
-        File file = temporaryFolder.newFile("histogram");
+        File file = Paths.get(tempDir.toString(), "histogram").toFile();
         writeString(file.toPath(), format("1: %d %d %s (%s)", instances, size, name, module));
         ToolContentDataLoader dataLoader = new ToolContentDataLoader(singletonList(new LightVirtualFile(file.getPath())));
         dataLoader.execute();
@@ -47,13 +48,9 @@ public class ToolContentDataLoaderTest {
 
     @Test
     public void differenceCalculator1() throws Exception {
-        List<VirtualFile> files = asList(
-                new LightVirtualFile(temporaryFolder.newFile("1").getPath()),
-                new LightVirtualFile(temporaryFolder.newFile("2").getPath()),
-                new LightVirtualFile(temporaryFolder.newFile("3").getPath()),
-                new LightVirtualFile(temporaryFolder.newFile("4").getPath()),
-                new LightVirtualFile(temporaryFolder.newFile("5").getPath()),
-                new LightVirtualFile(temporaryFolder.newFile("6").getPath()));
+        List<VirtualFile> files = range(1, 7)
+                .mapToObj(index -> new LightVirtualFile(Paths.get(tempDir.toString(), Integer.toString(index)).toString()))
+                .collect(toList());
         writeString(Paths.get(files.get(0).getPath()), "1: 10 20 testName");
         writeString(Paths.get(files.get(1).getPath()), "");
         writeString(Paths.get(files.get(2).getPath()), "1: 5 10 testName");
@@ -77,10 +74,9 @@ public class ToolContentDataLoaderTest {
 
     @Test
     public void differenceCalculator2() throws Exception {
-        List<VirtualFile> files = asList(
-                new LightVirtualFile(temporaryFolder.newFile("1").getPath()),
-                new LightVirtualFile(temporaryFolder.newFile("2").getPath()),
-                new LightVirtualFile(temporaryFolder.newFile("3").getPath()));
+        List<VirtualFile> files = range(1, 4)
+                .mapToObj(index -> new LightVirtualFile(Paths.get(tempDir.toString(), Integer.toString(index)).toString()))
+                .collect(toList());
         writeString(Paths.get(files.get(0).getPath()), "");
         writeString(Paths.get(files.get(1).getPath()), "1: 10 20 testName");
         writeString(Paths.get(files.get(2).getPath()), "1: 20 40 testName");
